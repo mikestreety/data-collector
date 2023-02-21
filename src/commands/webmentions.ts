166ed87893340c35ea49d64b881424d346136e93
Mike Street <mike@liquidlight.co.uk>
2023-02-21 10:23:11 +0000

    Add more documentation

--- src/commands/webmentions.ts
+++ src/commands/webmentions.ts
@@ -7,33 +7,44 @@ export default class Webmentions extends Command {
 	static description = 'Retrieve webmentions for a given URL'
 
 	static examples = [
-		'<%= config.bin %> <%= command.id %>',
+		'<%= config.bin %> <%= command.id %> webmentions.json -d www.mikestreety.co.uk -t 123456',
 	]
 
 	static flags = {
 		domain: Flags.string({
 			char: 'd',
+			required: true,
 			description: 'The domain you want the webmentions for'
 		}),
 		token: Flags.string({
 			char: 't',
+			required: true,
 			description: 'Your token from webmentions.io'
 		}),
 		perPage: Flags.string({
-			description: 'How many per page',
-			default: '500'
+			default: '500',
+			description: 'How many per page'
 		}),
 		sortBy: Flags.string({
-			description: 'Your token from webmentions.io'
+			description: 'Sorting mechanism',
+			default: 'created',
+			options: ['created', 'updated', 'published', 'rsvp']
 		}),
 		sortDir: Flags.string({
-			description: 'Your token from webmentions.io'
+			description: 'Ordering direction',
+			default: 'down',
+			options: ['down', 'up']
 		}),
 		property: Flags.string({
-			description: 'Your token from webmentions.io'
+			description: 'Limit returned webmentions with specific properties',
+			options: ['in-reply-to', 'like-of', 'repost-of', 'bookmark-of', 'mention-of', 'rsvp'],
+			multiple: true
 		}),
 		since: Flags.string({
-			description: 'Your token from webmentions.io'
+			description: 'Specify date to find new mentions retrieved by the service since.'
+		}),
+		sinceId: Flags.string({
+			description: 'Specify ID to find new mentions retrieved by the service since.'
 		}),
 	}
 
