c3bfce46f4a673a7e1f5b740ed03fefa6f1e4f0e
Mike Street <mike@liquidlight.co.uk>
2023-03-01 11:19:18 +0000

    Allow config to be passed to parser

--- src/utils/Rss.ts
+++ src/utils/Rss.ts
@@ -1,7 +1,7 @@
 import * as Parser from 'rss-parser';
 
-export async function fetchRssFeed(options: any) {
-	const parser = new Parser();
+export async function fetchRssFeed(options: any, config: any = []) {
+	const parser = new Parser(config ?? {});
 	const feed = await parser.parseURL(options.feed);
 	return feed.items;
 }
