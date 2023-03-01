2571b394ac9d1241dd6927e9dda245b995f280d5
Mike Street <mike@liquidlight.co.uk>
2023-03-01 11:19:28 +0000

    Extract letterboxed meta

--- src/commands/letterboxd.ts
+++ src/commands/letterboxd.ts
@@ -35,36 +35,34 @@ export default class Letterboxd extends Command {
 
 		// Get all the webmentions required
 		flags.feed = `https://letterboxd.com/${flags.username}/rss/`
-		const feed = await u.fetchRssFeed(flags);
+		const feed = await u.fetchRssFeed(
+			flags,
+			{
+				customFields: {
+					item: [
+						'letterboxd:filmTitle',
+						'letterboxd:watchedDate',
+						'letterboxd:filmYear',
+						'letterboxd:memberRating',
+						'letterboxd:rewatch',
+					],
+				}
+			}
+		);
 
 		feed
 			// Filter out lists
 			.filter(item => item.guid?.includes('letterboxd-watch'))
-			// Add title, year and rating
 			.map(item => {
-				// The title is formed of [Film, Year - Rating]
-				// Split on the hyphen as it may have commas in the film title
-				let details = item.title?.split('-');
-
-				if(details?.length == 2) {
-					let rating = details[1].trim(),
-						halfRating = 0;
-
-					if (rating.includes('½')) {
-						rating = rating.replace('½', '');
-						halfRating = 0.5;
+				for(const key in item) {
+					let keyParts = key.split(':');
+					if (keyParts.length == 2) {
+						item[keyParts[1]] = item[key];
+						delete item[key];
 					}
-					item.rating = `${rating.length + halfRating}`;
-
-					let title = details[0].trim();
-					// The year is the last 4 characters
-					item.filmYear = title.substring(title.length - 4);
-					// The film has a title then a comma<space><year>
-					item.filmTitle = title.substring(0, title.length - 6).trim();
 				}
-
 				return item;
-			})
+			});
 
 		// If we have them save them
 		if (feed) {
